2c437cec6848d6eaee6ca039206a1e21ece11cf2
Daniel Casper <dc@danielcasper.com>
2015-02-25 01:03:18 -0600

    Exposes assets via route

---
+++ __tests__/test.js
@@ -0,0 +1,7 @@
+//jest.dontMock('../app/react_components/main.jsx');
+
+/*describe('test', function() {
+ it('adds 1 + 2 to equal 3', function() {
+   expect(1 + 2).toBe(3);
+ });
+});*/
--- server.js
+++ server.js
@@ -1,28 +1,65 @@
-var config = require('./config');
-var Hapi = require('hapi');
-var Good = require('good');
+var http = require('http'),
+    browserify = require('browserify'),
+    literalify = require('literalify'),
+    React = require('react'),
+    Hapi = require('hapi'),
+    url = require('url'),
+    path = require("path"),
+    Good = require('good'),
+    config = require('./config');
+
 var server = new Hapi.Server();
 
 server.connection({ port: config.port});
 
-server.route({
-	method: '*',
-	path: '/{p*}',
-	handler: function(request, reply) {
-		return reply("404'ed! Why you come here?").code(404);
-	}
-}); 
-
+/* Directory routing 
 server.route({
   method: 'GET',
   path: '/{p*}',
-  handler: {
+  handler:  {
+
     directory: {
       path: './public',
       listing: false,
       index: true
     }
   }
+});*/
+
+server.route({
+  method: 'GET',
+  path: '/{p*}',
+handler: function(request, reply) {
+      if (request.url.path == '/') {
+          reply.file('./public/index.html')
+      } else if (request.url.path == '/vendor.js') {
+          reply.file(__dirname+'/public/'+request.url.path)
+      } else if (request.url.path == '/bundle.js') {
+          reply.file(__dirname+'/public/'+request.url.path)
+      } else {
+        reply("404'ed! Why you come here?").code(404);
+      } 
+   }
+});
+
+//expose assets
+server.route({
+	method: 'GET',
+	path: '/assets/{p*}',
+	handler: {
+		directory: {
+			path: './public/assets'
+		}
+	}
+});
+
+//add 404 for the rest of the stuff later
+server.route({
+	method: '*',
+	path: '/{p*}',
+	handler: function(request, reply){
+
+	}
 });
 
 server.register({
